14de9ab0ba1f6f06a4f93d23cdeb0b9a349be536
Lucas Ferreira <lucasferreirasouza22@gmail.com>
2026-01-08 21:59:02 -0300

    forEach

---
+++ interacao.js
@@ -0,0 +1,14 @@
+function criarBotoes() {
+    const itens = document.querySelectorAll("li")
+    const conteudos = ["Texto", "Vídeo", "Código"]
+
+    itens.forEach(li => {
+        conteudos.forEach(valor => {
+            const botao = document.createElement("button")
+            botao.textContent = valor
+            li.appendChild(botao)
+        })
+    })
+}
+
+criarBotoes()
